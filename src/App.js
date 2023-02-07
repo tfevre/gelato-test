983ef45f69e43c55ff2e56d03b04b0533f57119f
tfevre <tfevre@metadev3.com>
2023-02-07 15:14:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { ethers } from "ethers";
+import { GelatoRelay, SponsoredCallERC2771Request } from "@gelatonetwork/relay-sdk";
+const relay = new GelatoRelay();
 
 function App() {
+  async function test(){
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const accounts = await provider.send("eth_requestAccounts", []);
+    
+    // Set up on-chain variables, such as target address
+    const counter = "0x01A9B3d53b009A1c9Ea01589235848c5e2bAD2e4"; 
+    const abi = ["function captureTheFlag()"];
+    
+    const signer = provider.getSigner();
+    const user = signer.getAddress();
+
+    // Generate the target payload
+    const contract = new ethers.Contract(counter, abi, signer);
+    const { data } = await contract.populateTransaction.captureTheFlag();
+
+    // Populate a relay request
+    const request = {
+      chainId: provider.network.chainId,
+      target: counter,
+      data: data,
+      user: await user,
+    };
+    console.log(request)
+    // Without a specific API key, the relay request will fail! 
+    // Go to https://relay.gelato.network to get a testnet API key with 1Balance.
+    // Send a relay request using Gelato Relay!
+    const apiKey = 'PIEXdwAXAf3Yna4yZcOi_MAv7IBs3UfSqiNDM9n2Pi0_';
+    const relayResponse = await relay.sponsoredCallERC2771(request, provider, apiKey);
+  }
+  
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +43,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={test}>Test</button>
       </header>
     </div>
   );
